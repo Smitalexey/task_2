95ee6a4bc879bd598c3668bbc63b28205a26c036
Aleksey <smit_alexey@mail.ru>
2022-08-14 19:40:04 +0500

    Выводим третью цифру заданного числа

---
+++ task2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System; //попробовать решить 2 задачу
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите любое натуральное число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        string numberText = Convert.ToString(number);
+        if (numberText.Length > 2)
+        {
+            Console.WriteLine("третья цифра числа - " + numberText[2]);
+            }
+            else
+            {
+                Console.WriteLine("третьей цифры нет");
+            }
+    }
+}
